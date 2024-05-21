79289951b1c66500826442c5add0a67b27703cda
Faruk Kodat <farukkodat@gmail.com>
2024-05-21 23:40:51 +0300

    First initial commit

---
+++ movie-network/src/main/java/com/kodat/of/movie/MovieNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.kodat.of.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNetworkApplication.class, args);
+    }
+
+}
---
+++ movie-network/src/test/java/com/kodat/of/movie/MovieNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodat.of.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
