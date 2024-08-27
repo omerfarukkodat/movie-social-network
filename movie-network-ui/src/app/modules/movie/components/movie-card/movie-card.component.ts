526dcf6e42c1358f1216fd61666736cac17b6702
Faruk Kodat <farukkodat@gmail.com>
2024-08-27 13:29:32 +0300

    implemented movie-card

--- movie-network-ui/src/app/modules/movie/components/movie-card/movie-card.component.ts
+++ movie-network-ui/src/app/modules/movie/components/movie-card/movie-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MovieResponse} from "../../../../services/models/movie-response";
 
 @Component({
@@ -10,7 +10,10 @@ export class MovieCardComponent {
 
 
   private _movie: MovieResponse = {};
+  private _manage = false;
   private _movieCover: string | undefined;
+
+
 
 
   get movie(): MovieResponse {
@@ -24,8 +27,53 @@ export class MovieCardComponent {
 
   get movieCover(): string | undefined {
     if (this._movie.movieCover){
-      return '';
+      return 'data:image/jpg;base64, ' + this._movie.movieCover;
     }
     return this._movieCover;
   }
+
+  get manage(): boolean {
+    return this._manage;
+  }
+@Input()
+  set manage(value: boolean) {
+    this._manage = value;
+  }
+
+  @Output() private share: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+  @Output() private archive: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+  @Output() private addToWaitingList: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+  @Output() private borrow: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+  @Output() private edit: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+  @Output() private details: EventEmitter<MovieResponse> = new EventEmitter<MovieResponse>();
+
+  onShowDetails() {
+    this.details.emit(this._movie)
+  }
+
+  onAddToWaitingList() {
+  this.addToWaitingList.emit(this._movie)
+  }
+
+  onBorrow() {
+  this.borrow.emit(this._movie)
+  }
+
+  onEdit() {
+  this.edit.emit(this._movie)
+  }
+
+  onShare() {
+this.share.emit(this._movie)
+  }
+
+  onArchive() {
+this.archive.emit(this._movie)
+  }
+
+
+
+
+
+
 }
